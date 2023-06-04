8b1f9ed0a5764ac3d61579fa685943d177e6b878
Robert Niemela <rvniemela@hotmail.com>
2023-06-04 11:43:50 -0700

    add styling

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require("express"); // Import Express.js module
-const app = express(); // Create an instance of Express
-app.use(express.json()); // Use the built-in JSON middleware to parse JSON requests
-app.use(express.urlencoded({ extended: true })); // Use the built-in middleware to parse URL-encoded requests
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let appText = ""; // Variable to hold the text
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", "ejs"); // Set EJS as the view engine for Express
+// Serve static files from the "node_modules" directory
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
-// Define a route for the root URL ("/") and render the index page with the current text
+let appText = "";
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.render("index", { appText: appText });
 });
 
-// Define a route for saving the text
 app.post("/save", (req, res) => {
-  appText = req.body.appText; // Update the text from the request body
-  res.redirect("/"); // Redirect back to the index page
+  appText = req.body.appText;
+  res.redirect("/");
 });
 
-// Define a route for the API endpoint
 app.get("/api/hello", (req, res) => {
-  res.json({ appText: appText }); // Return the current text as a JSON object
+  res.json({ appText: appText });
 });
 
-// Start the server on port 3000
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
